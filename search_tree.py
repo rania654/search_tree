59abc3a529cafa9aab388b11ac45a0540c0c2909
rania654 <raniayusuf12@outlook.com>
2026-03-28 16:36:38 +0000

    Add files via upload

--- search_tree.py
+++ search_tree.py
@@ -1,4 +1,4 @@
-user = input("Say a number: ")
+user = int(input("Say a number: "))
 
 
 class treenode():
@@ -6,38 +6,24 @@ class treenode():
         self.data = data
         self.leftnode = None
         self.rightnode = None
-
-    def search_root(self,root):
-        if root.data == user:
-            print("Your number is the root!!")
-        else:
-            return
     
-    def search_leftnode(self,root):
+    def search(self,root):
         if root != None:
-            if root.leftnode.data == user:
-                print("Your number is in the left root!")
-                return
-            self.search_leftnode(root.leftnode)
+            if root.data == user:
+                print("Your number is in the tree!")
+                return True
+            self.search(root.leftnode)
+            self.search(root.rightnode)
         else:
-            return
-
-
-    def search_rightnode(self,root):
-        if root != None:
-            rightnodes = self.search_rightnode(root.rightnode)
-        elif rightnodes == user:
-            print("Your number is in the right root!")
-        else:
-            return
-
+            return False
+        
 
 tree = treenode(50)
 tree.leftnode = treenode(40)
 tree.leftnode.leftnode = treenode(20)
 tree.rightnode = treenode(30)
-tree.search_root(tree)
-tree.search_leftnode(tree)
-#tree.search_rightnode(tree)
+var = tree.search(tree)
+if var != True:
+    print("Your number is not in the tree!")
 
-        
+
